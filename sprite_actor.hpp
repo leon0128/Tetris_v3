5b860e45053d3bcaef0d0b639b12a8f923074239
leon0128 <leon0128cocoa@gmail.com>
2019-06-18 12:53:40 +0900

    [update] Create a SpriteActor class header.

---
+++ sprite_actor.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "actor.hpp"
+#include <SDL2/SDL.h>
+
+class SpriteActor : public Actor
+{
+public:
+    SpriteActor(class Game* game, int order);
+
+    // メンバ変数の設定、取得
+    void setTexture(SDL_Texture* texture){mTexture = texture;}
+    
+protected:
+    SDL_Texture* mTexture;
+};
