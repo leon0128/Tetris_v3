7ad591fa74f49912b65c00d127d14a61d882d9de
leon0128 <leon0128cocoa@gmail.com>
2019-06-19 01:45:04 +0900

    [update] Implementation of Block::updatePosition().

--- block.cpp
+++ block.cpp
@@ -1,4 +1,5 @@
 #include "block.hpp"
+#include "game_board.hpp"
 
 Block::Block(Game* game, 
              int order, 
@@ -11,4 +12,15 @@ Block::Block(Game* game,
     mCoordinate = new Vector2(coordinate->x, coordinate->y);
     // 大きさを合わせる
     setScale(BLOCK_SIZE / 60.0f);
+}
+
+void Block::updatePosition()
+{
+    Vector2* center = mGameBoard->getPosition();
+    mPosition->setX(center->x + 
+                    (mCoordinate->x - 5) * 
+                    BLOCK_SIZE - BLOCK_SIZE / 2);
+    mPosition->setY(center->y -
+                    (mCoordinate->x - 10) * 
+                    BLOCK_SIZE - BLOCK_SIZE / 2);
 }
--- block.hpp
+++ block.hpp
@@ -10,6 +10,9 @@ public:
           class GameBoard* gameBoald,
           class Vector2* coordinate);
 
+    // 座標を元にmGameBoardに対する適切な位置を計算
+    void updatePosition();
+
 private:
     class GameBoard* mGameBoard;
     class Vector2* mCoordinate;
