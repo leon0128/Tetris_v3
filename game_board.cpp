1bf4b2bc62604d89ec2f0ff38f67fe2c578f0f4e
leon0128 <leon0128cocoa@gmail.com>
2019-06-18 23:32:18 +0900

    [update] Initialize mGameState and mActiveMino.

--- game_board.cpp
+++ game_board.cpp
@@ -1,14 +1,22 @@
 #include "game_board.hpp"
 
 GameBoard::GameBoard(Game* game, int order):
-    SpriteActor(game, order)
+    SpriteActor(game, order),
+    mActiveTetrominio(nullptr)
 {
     // 自身の位置とテクスチャの設定
     mPosition = new Vector2(WINDOW_WIDTH * 3 / 8, WINDOW_HEIGHT / 2);
     setTexture(mGame->getTexture("image/game_board.png"));
+
+    // mGameStateの設定
+    for(int i = 0; i < GAMEBOARD_VERTICAL; i++)
+    {
+        std::array<class Block*, GAMEBOARD_PARALLEL> line = {nullptr};
+        mGameState.push_back(line);
+    }
 }
 
 void GameBoard::update(int cuurentFrame)
 {
-    
+
 }
--- game_board.hpp
+++ game_board.hpp
@@ -9,4 +9,10 @@ public:
 
     // いくつかの関数の実行
     void update(int currentFrame) override;
+
+private:
+    // ブロックを格納する配列
+    std::vector<std::array<class Block*, GAMEBOARD_PARALLEL>> mGameState;
+    // 操作するTetromino
+    class Tetromino* mActiveTetrominio;
 };
