a5829716dbfd49c7f7b1c8f82294779314205edc
leon0128 <leon0128cocoa@gmail.com>
2019-06-22 01:56:57 +0900

    [update] Implementation of SideBorad::setType() and draw().

--- side_board.cpp
+++ side_board.cpp
@@ -3,9 +3,76 @@
 SideBoard::SideBoard(Game* game, int order, GameBoard* gameBoard):
     SpriteActor(game, order),
     mGameBoard(gameBoard),
-    mType(NONE)
+    mTetrominoTexture(nullptr)
 {
     setTexture(mGame->getTexture("image/side_board.png"));
     setClear(0.65f);
 }
+
+void SideBoard::draw(SDL_Renderer* renderer)
+{
+    SpriteActor::draw(renderer);
     
+    if(mTetrominoTexture)
+    {
+        // 描画
+        SDL_RenderCopy(renderer,
+                       mTetrominoTexture,
+                       nullptr,
+                       &mTetrominoRectangle); 
+    }
+}
+
+void SideBoard::setType(EType type)
+{
+    switch(type)
+    {
+        case(I):
+            mTetrominoTexture = mGame->getTexture("image/tetromino/I_mino.png");
+            break;
+
+        case(O):
+            mTetrominoTexture = mGame->getTexture("image/tetromino/O_mino.png");
+            break;
+
+        case(T):
+            mTetrominoTexture = mGame->getTexture("image/tetromino/T_mino.png");
+            break;    
+
+        case(L):
+            mTetrominoTexture = mGame->getTexture("image/tetromino/L_mino.png");
+            break;
+
+        case(J):
+            mTetrominoTexture = mGame->getTexture("image/tetromino/J_mino.png");
+            break; 
+
+        case(S):
+            mTetrominoTexture = mGame->getTexture("image/tetromino/S_mino.png");
+            break; 
+
+        case(Z):
+            mTetrominoTexture = mGame->getTexture("image/tetromino/Z_mino.png");
+            break;
+        
+        case(NONE):
+            SDL_Log("Can not set Tetromino texture: SideBoard::setType()");
+            break;
+    }
+
+    if(mTetrominoTexture)
+    {
+        int width, height;
+        // mRectangle, mTextureSizeの初期化
+        SDL_QueryTexture(mTexture,
+                        nullptr,
+                        nullptr,
+                        &width,
+                        &height);
+        
+        mRectangle.w = width;
+        mRectangle.h = height;
+        mRectangle.x = mPosition.x - width / 2;
+        mRectangle.y = mPosition.y - height / 2;   
+    }
+}
--- side_board.hpp
+++ side_board.hpp
@@ -8,7 +8,16 @@ public:
     SideBoard(class Game* game,
               int order,
               class GameBoard* gameBoard);
+
+    void draw(SDL_Renderer* renderer) override;
+    
+    // 表示するテトロミノを設定
+    void setType(EType type);
+
 private:
     class GameBoard* mGameBoard;
-    enum EType mType;
+    
+    // 表示するテトロミノ用
+    SDL_Texture* mTetrominoTexture;
+    SDL_Rect mTetrominoRectangle;
 };
